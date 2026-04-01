f9a510e69d6831466c6ccaf8c9e297afbd71aeaa
Felipe Cardoso Martins <felipe.cardoso.martins@gmail.com>
2026-04-01 16:57:48 -0300

    chore: prettier format

--- tests/e2e/test_postgres.py
+++ tests/e2e/test_postgres.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import joinedload
+
 from tests.models import AlarmPanel, Sensor
 
 def test_insert_and_select_normalization(postgres_session, postgres_capquery):
-    # Action (Insert)
     panel = AlarmPanel(mac_address="00:11:22:33:44:55", is_online=True)
     sensor = Sensor(name="Front Door", sensor_type="Contact")
     panel.sensors.append(sensor)
@@ -10,22 +10,52 @@ def test_insert_and_select_normalization(postgres_session, postgres_capquery):
     postgres_session.add(panel)
     postgres_session.flush()
 
-    # Action (Select)
     queried_panel = postgres_session.query(AlarmPanel).options(joinedload(AlarmPanel.sensors)).filter_by(mac_address="00:11:22:33:44:55").first()
     assert queried_panel is not None
 
     postgres_capquery.assert_executed_queries(
         "BEGIN",
         (
-            "INSERT INTO alarm_panels (mac_address, is_online) VALUES (%(mac_address)s, %(is_online)s) RETURNING alarm_panels.id",
+            # language=SQL
+            """
+            INSERT INTO alarm_panels (mac_address, is_online)
+            VALUES (%(mac_address)s, %(is_online)s)
+            RETURNING alarm_panels.id
+            """,
             {"mac_address": "00:11:22:33:44:55", "is_online": True}
         ),
         (
-            "INSERT INTO sensors (panel_id, name, sensor_type) VALUES (%(panel_id)s, %(name)s, %(sensor_type)s) RETURNING sensors.id",
+            # language=SQL
+            """
+            INSERT INTO sensors (panel_id, name, sensor_type)
+            VALUES (%(panel_id)s, %(name)s, %(sensor_type)s)
+            RETURNING sensors.id
+            """,
             {"panel_id": 1, "name": "Front Door", "sensor_type": "Contact"}
         ),
         (
-            "SELECT anon_1.alarm_panels_id AS anon_1_alarm_panels_id, anon_1.alarm_panels_mac_address AS anon_1_alarm_panels_mac_address, anon_1.alarm_panels_is_online AS anon_1_alarm_panels_is_online, sensors_1.id AS sensors_1_id, sensors_1.panel_id AS sensors_1_panel_id, sensors_1.name AS sensors_1_name, sensors_1.sensor_type AS sensors_1_sensor_type \nFROM (SELECT alarm_panels.id AS alarm_panels_id, alarm_panels.mac_address AS alarm_panels_mac_address, alarm_panels.is_online AS alarm_panels_is_online \nFROM alarm_panels \nWHERE alarm_panels.mac_address = %(mac_address_1)s \n LIMIT %(param_1)s) AS anon_1 LEFT OUTER JOIN sensors AS sensors_1 ON anon_1.alarm_panels_id = sensors_1.panel_id",
+            # language=SQL
+            """
+            SELECT
+                anon_1.alarm_panels_id AS anon_1_alarm_panels_id,
+                anon_1.alarm_panels_mac_address AS anon_1_alarm_panels_mac_address,
+                anon_1.alarm_panels_is_online AS anon_1_alarm_panels_is_online,
+                sensors_1.id AS sensors_1_id,
+                sensors_1.panel_id AS sensors_1_panel_id,
+                sensors_1.name AS sensors_1_name,
+                sensors_1.sensor_type AS sensors_1_sensor_type
+            FROM (
+                SELECT
+                    alarm_panels.id AS alarm_panels_id,
+                    alarm_panels.mac_address AS alarm_panels_mac_address,
+                    alarm_panels.is_online AS alarm_panels_is_online
+                FROM alarm_panels
+                WHERE alarm_panels.mac_address = %(mac_address_1)s
+                LIMIT %(param_1)s
+            ) AS anon_1
+            LEFT OUTER JOIN sensors AS sensors_1
+                ON anon_1.alarm_panels_id = sensors_1.panel_id
+            """,
             {"mac_address_1": "00:11:22:33:44:55", "param_1": 1}
         ),
         strict=False
--- tests/e2e/test_sqlite.py
+++ tests/e2e/test_sqlite.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import joinedload
+
 from tests.models import AlarmPanel, Sensor
 
 def test_insert_and_select_normalization(sqlite_session, sqlite_capquery):
-    # Action (Insert)
     panel = AlarmPanel(mac_address="00:11:22:33:44:55", is_online=True)
     sensor = Sensor(name="Front Door", sensor_type="Contact")
     panel.sensors.append(sensor)
@@ -10,22 +10,50 @@ def test_insert_and_select_normalization(sqlite_session, sqlite_capquery):
     sqlite_session.add(panel)
     sqlite_session.flush()
 
-    # Action (Select)
     queried_panel = sqlite_session.query(AlarmPanel).options(joinedload(AlarmPanel.sensors)).filter_by(mac_address="00:11:22:33:44:55").first()
     assert queried_panel is not None
 
     sqlite_capquery.assert_executed_queries(
         "BEGIN",
         (
-            "INSERT INTO alarm_panels (mac_address, is_online) VALUES (?, ?)",
+            # language=SQL
+            """
+            INSERT INTO alarm_panels (mac_address, is_online)
+            VALUES (?, ?)
+            """,
             ("00:11:22:33:44:55", True)
         ),
         (
-            "INSERT INTO sensors (panel_id, name, sensor_type) VALUES (?, ?, ?)",
+            # language=SQL
+            """
+            INSERT INTO sensors (panel_id, name, sensor_type)
+            VALUES (?, ?, ?)
+            """,
             (1, "Front Door", "Contact")
         ),
         (
-            "SELECT anon_1.alarm_panels_id AS anon_1_alarm_panels_id, anon_1.alarm_panels_mac_address AS anon_1_alarm_panels_mac_address, anon_1.alarm_panels_is_online AS anon_1_alarm_panels_is_online, sensors_1.id AS sensors_1_id, sensors_1.panel_id AS sensors_1_panel_id, sensors_1.name AS sensors_1_name, sensors_1.sensor_type AS sensors_1_sensor_type \nFROM (SELECT alarm_panels.id AS alarm_panels_id, alarm_panels.mac_address AS alarm_panels_mac_address, alarm_panels.is_online AS alarm_panels_is_online \nFROM alarm_panels \nWHERE alarm_panels.mac_address = ?\n LIMIT ? OFFSET ?) AS anon_1 LEFT OUTER JOIN sensors AS sensors_1 ON anon_1.alarm_panels_id = sensors_1.panel_id",
+            # language=SQL
+            """
+            SELECT
+                anon_1.alarm_panels_id AS anon_1_alarm_panels_id,
+                anon_1.alarm_panels_mac_address AS anon_1_alarm_panels_mac_address,
+                anon_1.alarm_panels_is_online AS anon_1_alarm_panels_is_online,
+                sensors_1.id AS sensors_1_id,
+                sensors_1.panel_id AS sensors_1_panel_id,
+                sensors_1.name AS sensors_1_name,
+                sensors_1.sensor_type AS sensors_1_sensor_type
+            FROM (
+                SELECT
+                    alarm_panels.id AS alarm_panels_id,
+                    alarm_panels.mac_address AS alarm_panels_mac_address,
+                    alarm_panels.is_online AS alarm_panels_is_online
+                FROM alarm_panels
+                WHERE alarm_panels.mac_address = ?
+                LIMIT ? OFFSET ?
+            ) AS anon_1
+            LEFT OUTER JOIN sensors AS sensors_1
+                ON anon_1.alarm_panels_id = sensors_1.panel_id
+            """,
             ("00:11:22:33:44:55", 1, 0)
         ),
         strict=False
